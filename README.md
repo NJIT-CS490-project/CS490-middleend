283dcc1d7f580220b6a147654b451eeea3d2cbfa
Eduardo Preciado <ejp9@njit.edu>
2016-02-20 17:39:12 -0500

    Completed beta build

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #CS490-middleend
 
-__Replace me with real content, Eddo!__
+__Nick Farley Sucks Eggs__
---
+++ auth.php
@@ -0,0 +1,45 @@
+<?php
+	//Gets input from frontend request and decodes it
+	$request = file_get_contents('php://input');
+	$recieve = json_decode($request);
+	//Array for NJIT and DB login results
+	$results = array();
+	
+	//Store username and password in variables
+	$user = $recieve["user"];
+	$pass =	$recieve["pass"];
+	
+	//Create array of fields needed for each login
+	$db_fields = array("user" => $user, "pass" => $pass);
+	$njit_fields = array("user" => $user, "pass" => $pass, "uuid" => "0xACA021");
+
+	$ch1 = curl_init();
+	$ch2 = curl_init();
+	//Each url needed for logins
+	$db_url = "http://web.njit.edu/~mjc55/..";
+	$njit_url = "https://cp4.njit.edu/cp/home/login";
+
+	curl_setopt($ch1, CURLOPT_URL, $db_url);
+	curl_setopt($ch1, CURLOPT_POST, 1);
+	curl_setopt($ch1, CURLOPT_POSTFIELDS, http_build_query(db_fields));
+	curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1);
+	$db_result = curl_exec($ch1);
+	
+	//Stores result from cURL to DB
+	$results["db"] = $db_result;
+
+	curl_close($ch1);
+
+	curl_setopt($ch2, CURLOPT_URL, $njit_url);
+	curl_setopt($ch2, CURLOPT_POST, 1);
+	curl_setopt($ch2, CURLOPT_POSTFIELDS, http_build_query($njit_fields));
+	curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+	$njit_result = curl_exec($ch2);
+	
+	//Stores result from cURL to NJIT
+	$results["njit"] = strpos($njit_result, "loginok.html") !== false;
+
+	curl_close($ch2);
+	
+	echo json_encode($results);
+?>
