20eca7ea33b8fad1caad6fb0f95039978dc1f819
Eduardo J Preciado unknown student <ejp9@osl84.njit.edu>
2016-04-01 17:52:12 -0400

    Fix delete method

--- delete.php
+++ delete.php
@@ -1,8 +1,5 @@
 <?php
-	$request = file_get_contents("php://input");
-  $recieve = json_decode($request, true);
-
-	$id = $recieve["id"];
+	$id = $_GET["id"];
 
 	$db_fields = json_encode(array("id" => $id));
 	$ch = curl_init();
@@ -12,11 +9,20 @@
     $headers = getallheaders();
 
 	curl_setopt($ch, CURLOPT_URL, $db_url);
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $db_fields);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Cookie: ' . $headers['Cookie']));
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_exec($ch);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $db_fields);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Cookie: ' . $headers['Cookie']));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_exec($ch);
+
+    
+
+    if(curl_getinfo($ch)['http_code'] === 304){
+        http_response_code(400);
+    }
+    else {
+        http_response_code(200);
+    }
 
 	curl_close($ch);
 
