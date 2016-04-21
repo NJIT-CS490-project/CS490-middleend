669f766b6f05a53e6e8c31e3d536e25fa1b51f40
Eduardo J. Preciado <ejp9@njit.edu>
2016-04-21 17:36:54 -0400

    Update parsing of NJIT events

--- pull.php
+++ pull.php
@@ -5,6 +5,7 @@ $lines = file('http://25livepub.collegenet.com/calendars/NJIT_EVENTS.ics');
 $events = array();
 $temp = array();
 $adding = false;
+$NJIT = array();
 
     foreach ($lines as $line_num => $line) {
         if(strcmp($line, 'BEGIN:VEVENT') === 2) {
@@ -24,10 +25,40 @@ $adding = false;
     }
 
     foreach($events as $event) {
-        echo "<h3>New Event</h3>";    
+        $temp = "";
+        $currEvent = array();
         foreach($event as $info) {
-            echo "$info <br>";
+                if ($info[0] === " "){
+                        $temp = $temp . $info;
+                        continue;
+                } 
+                if($temp !== ""){
+                        $info = $temp;
+                        $temp = "";
+                }
+                if(strpos($info,"SUMMARY:") !== false){
+                        $currEvent["name"] = substr($info,8,-1);
+                }
+                elseif(strpos($info,"LOCATION:") !== false){
+                        $currEvent["location"] = substr($info,9,-1);
+                }
+                elseif(strpos($info,"DTSTART") !== false){
+                        $currEvent["start"] = substr($info,8,-1);
+                }
+                elseif(strpos($info,"DTEND") !== false){
+                        $currEvent["end"] = substr($info,6,-1);
+                }        
+                elseif(strpos($info,"UID:") !== false){
+                        $currEvent["id"] = $info;
+                }        
         }
+        array_push($NJIT, $currEvent);
     }
-
+        
+        foreach($NJIT as $event){
+                foreach($event as $key => $value){
+                        echo "$key $value <br>";
+                }
+        }
+        
 ?>
