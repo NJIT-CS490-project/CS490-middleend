d54da80b4109f3e211a946eb0db58feb1eb499f1
Eduardo J. Preciado <ejp9@njit.edu>
2016-04-22 03:33:34 -0400

    Add support for pagination

--- search.php
+++ search.php
@@ -3,9 +3,9 @@
   $results = array();
 
 	$query = $_GET["string"];
-	$count = (empty($_GET["count"])) ? 10 : $recieve["count"];
-	$offset = (empty($_GET["offset"])) ? 0 : $recieve["offset"];
-    
+	$count = (empty($_GET["count"])) ? 10 : $_GET["count"];
+	$offset = (empty($_GET["offset"])) ? 0 : $_GET["offset"];
+
 	$ch = curl_init();
 
 	$db_url = "https://web.njit.edu/~mjc55/CS490/event/search.php?query=$query";
@@ -20,15 +20,13 @@
 
 	if(empty($db_result)){
         
-                $results["db"] = false;
 		$results["message"] = "No events found";
 	}
         
         else{
                 
-                $results["db"] = true;
-		$results["message"] = "Events found";
-		$results["events"] = $db_result;
+                $results["events"] = array_slice($db_result, $offset, $count);
+                $results["hasNext"] = (sizeof($db_result) > ($offset+$count));
 	}
 
 	curl_close($ch);
