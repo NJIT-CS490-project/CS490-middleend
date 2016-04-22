3af86ba56d2626e153e1f101c0b79162f8e5fe07
Eduardo J. Preciado <ejp9@njit.edu>
2016-04-22 03:43:59 -0400

    Update values for sorting

--- search.php
+++ search.php
@@ -5,6 +5,8 @@
 	$query = $_GET["string"];
 	$count = (empty($_GET["count"])) ? 10 : $_GET["count"];
 	$offset = (empty($_GET["offset"])) ? 0 : $_GET["offset"];
+        $order = (empty($_GET["order"])) ? "asc" : $_GET["order"];
+        $sorting = (empty($_GET["sorting"])) ? "title" : $_GET["sorting"];
 
 	$ch = curl_init();
 
@@ -26,7 +28,7 @@
         else{
                 
                 $results["events"] = array_slice($db_result, $offset, $count);
-                $results["hasNext"] = (sizeof($db_result) > ($offset+$count));
+                $results["done"] = !(sizeof($db_result) > ($offset+$count));
 	}
 
 	curl_close($ch);
