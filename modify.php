d2e07e3e035b4fea82a9dc1b646a3bd9662bd336
Eduardo J. Preciado <ejp9@njit.edu>
2016-05-06 16:03:55 -0400

    Fix modification of events

--- modify.php
+++ modify.php
@@ -2,37 +2,39 @@
     $request = file_get_contents("php://input");
     $recieve = json_decode($request, true);
     
-    $description = $recieve['description'];
     $startTime   = $recieve['startTime'];
     $endTime     = $recieve['endTime'];
     $building    = $recieve['building'];
     $room        = $recieve['room'];
     $id          = $recieve['id'];
+    $date        = $recieve['date'];
+
+    $ch = curl_init();
 
    //Update URL when endpoint is up 
     $db_url = "https://web.njit.edu/~mjc55/CS490/event/update.php";
 
+    $headers = getallheaders();
     //Update db_fields when endpoint is up
-    $db_fields = array("description" => $description, "startTime" => $startTime, "endTime" => $endTime, "building" => $building, "room" => $room, "id" => $id);
+    $db_fields = json_encode(array("startTime" => $startTime, "endTime" => $endTime, "building" => $building, "room" => $room, "id" => $id));
     $header_fields = array('Content-Type: application/json', 'Cookie: ' . $headers['Cookie']);
     $response = array();
-    $headers = getallheaders();
+
 
     curl_setopt($ch, CURLOPT_URL, $db_url);
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $db_fields);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $header_fields);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-
-    //Need to fix logic once endpoint is put up
-    if(curl_getinfo($ch)['http_code'] !== 200) {
-        http_response_code(400);
-        $reponse['message'] = "Failed to modify event";
+    $db_result = json_decode(curl_exec($ch), true);
+    
+    if(curl_getinfo($ch)['http_code'] === 200) {
+        http_response_code(200);
+        $reponse['message'] = "Successfully modified event";
     }
     else {
-        http_response_code(200);
-        $response['message'] = "Successfully modified event";   
+        http_response_code(400);
+        $response['message'] = $db_result['message'];   
     }
 
     curl_close($ch);
